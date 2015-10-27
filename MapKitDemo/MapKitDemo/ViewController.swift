1b6157485489f60f958e6f38a97a1872ef99c071
Alicia Sykes <alicia@aliciasykes.com>
2015-10-27 09:51:28 +0000

    Added map and linked to VC

--- MapKitDemo/MapKitDemo/ViewController.swift
+++ MapKitDemo/MapKitDemo/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let initialLocation = CLLocation(latitude: 51.7504642, longitude: -1.282693)
+    
+        centerMapOnLocation(initialLocation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
 }
 
